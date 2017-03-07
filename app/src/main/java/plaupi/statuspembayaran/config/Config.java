41d647b36e3be5300d3f51146e529615deebde87
azizashari <aziz.ashari1@gmail.com>
2017-03-07 18:01:09 +0700

    First

---
+++ app/src/main/java/plaupi/statuspembayaran/config/Config.java
@@ -0,0 +1,19 @@
+package plaupi.statuspembayaran.config;
+
+/**
+ * Created by Aziz on 05/03/2017.
+ */
+
+public class Config {
+
+    public static final String URL = "http://sipeja.pe.hu/";
+
+    //JSON for load status keuangan
+    public static final String JSON_ARRAY = "result";
+    public static final String KEY_LUNAS = "Lunas"; //total lunas
+    public static final String KEY_BELUMLUNAS = "BelumLunas"; //total belum lunas
+    public static final String KEY_LABS = "nama"; //nama laboratorium
+    public static final String KEY_TAHUN = "tahun";
+
+}
+
